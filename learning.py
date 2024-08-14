872275a512830992ab0db6b3e728d5c5783f441c
Danny-Halstead <dannyrhalstead@gmail.com>
2024-08-14 17:03:14 -0500

    frame with buton using frame

--- learning.py
+++ learning.py
@@ -4,16 +4,14 @@ def main():
     root = tk.Tk()
     root.title('Simple App')
 
+    frame = tk.Frame(root)
+    frame.grid(row=0, column=0)
 
-    def on_click():
-        print('Testing')
+    entry = tk.Entry(frame)
+    entry.grid(row=0, column=0)
 
-
-    lbl = tk.Label(root, text='Label 1')
-    lbl.grid(row = 0, column = 0)
-
-    btn = tk.Button(root, text='Button 1', command=on_click)
-    btn.grid(row = 0, column = 1)
+    entry_btn = tk.Button(frame, text='Add')
+    entry_btn.grid(row=0, column=0)
 
     root.mainloop()
 
