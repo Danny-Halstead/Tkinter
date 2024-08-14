a5a1277a5c7738d7e778a02d0efae42254616823
Danny-Halstead <dannyrhalstead@gmail.com>
2024-08-14 17:34:39 -0500

    Grows properly when resized!

--- learning.py
+++ learning.py
@@ -10,11 +10,17 @@ def main():
             text_list.insert(tk.END, text)
             entry.delete(0, tk.END)
 
+    root.columnconfigure(0, weight=1)
+    root.rowconfigure(0, weight=1)
+
     frame = tk.Frame(root)
-    frame.grid(row=0, column=0)
+    frame.grid(row=0, column=0, sticky='nsew')
+
+    frame.columnconfigure(0, weight=1)
+    frame.rowconfigure(1, weight=1)
 
     entry = tk.Entry(frame)
-    entry.grid(row=0, column=0)
+    entry.grid(row=0, column=0, sticky='ew')
 
     entry.bind("<Return>", lambda event: add_to_list())
 
@@ -22,7 +28,7 @@ def main():
     entry_btn.grid(row=0, column=1)
 
     text_list = tk.Listbox(frame)
-    text_list.grid(row=1, column=0, columnspan=2, sticky='ew')
+    text_list.grid(row=1, column=0, columnspan=2, sticky='nsew')
 
     root.mainloop()
 
