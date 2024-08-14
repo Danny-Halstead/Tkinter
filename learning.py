bab071107cff792b4bc798798a331f3de92ee130
Danny-Halstead <dannyrhalstead@gmail.com>
2024-08-14 17:21:52 -0500

    Add elements to list w. enter key

--- learning.py
+++ learning.py
@@ -4,7 +4,7 @@ def main():
     root = tk.Tk()
     root.title('Simple App')
 
-    def add_to_list():
+    def add_to_list(event=None):
         text = entry.get()
         if text:
             text_list.insert(tk.END, text)
@@ -15,6 +15,8 @@ def main():
 
     entry = tk.Entry(frame)
     entry.grid(row=0, column=0)
+
+    entry.bind("<Return>", lambda event: add_to_list())
 
     entry_btn = tk.Button(frame, text='Add', command = add_to_list)
     entry_btn.grid(row=0, column=1)
