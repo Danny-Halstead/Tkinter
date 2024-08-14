c5ca8b73b60fc731023ec88089e5d7c0681fc530
Danny-Halstead <dannyrhalstead@gmail.com>
2024-08-14 16:49:34 -0500

    Label and button

--- learning.py
+++ learning.py
@@ -4,6 +4,12 @@ def main():
     root = tk.Tk()
     root.title('Simple App')
 
+    lbl = tk.Label(root, text='Label 1')
+    lbl.grid(row = 0, column = 0)
+
+    btn = tk.Button(root, text='Button 1')
+    btn.grid(row = 0, column = 1)
+
     root.mainloop()
 
 if __name__ == "__main__":
