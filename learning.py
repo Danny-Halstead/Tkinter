5a34480ee0fe9d7c83d645401ecfcd85817a2916
Danny-Halstead <dannyrhalstead@gmail.com>
2024-08-14 16:51:50 -0500

    Clickable button

--- learning.py
+++ learning.py
@@ -4,10 +4,15 @@ def main():
     root = tk.Tk()
     root.title('Simple App')
 
+
+    def on_click():
+        print('Testing')
+
+
     lbl = tk.Label(root, text='Label 1')
     lbl.grid(row = 0, column = 0)
 
-    btn = tk.Button(root, text='Button 1')
+    btn = tk.Button(root, text='Button 1', command=on_click)
     btn.grid(row = 0, column = 1)
 
     root.mainloop()
