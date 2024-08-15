60c96e5b4863dc283043a151df8444da2e82b214
Danny-Halstead <dannyrhalstead@gmail.com>
2024-08-15 16:40:37 -0500

    Set up blank window with classes and main function

--- learning.py
+++ learning.py
@@ -1,14 +1,19 @@
 import tkinter as tk
 from tkinter import ttk
 
+def main():
+    app = Application()
+    app.mainloop()
+
 class Application(tk.Tk):
     def __init__(self):
         super().__init__()
         self.title('Simple App')
 
 
-app = Application()
-app.mainloop()
+
+if __name__ == "__main__":
+    main()
 
 '''
 def main():
